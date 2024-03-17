e0ce11592f4baf870a07b6613e708b5f34c01065
RedZeroTheCat <146474467+RedZeroTheCat@users.noreply.github.com>
2024-03-17 23:19:40 +0300

    Upload

--- Lab1/equations.fsx
+++ Lab1/equations.fsx
@@ -1,26 +1,66 @@
 // Определите функции для решение алгебраических уравнений
 
-let dichotomy f a b = 0.
+let rec dichotomy f (a: float) (b: float) =
+    let eps = 0.000001
+    let xn = (a + b) / 2.
+    let fa = f a
+    let fb = f b
 
-let iterations phi x0 = 0.
+    if abs (f xn) < eps then
+        xn
+    else if fa < fb then
+        if (f xn) < 0. then dichotomy f xn b else dichotomy f a xn
+    else 
+        if (f xn) > 0. then dichotomy f xn b else dichotomy f a xn
 
-let newthon f f' x0 = 0.
+
+let rec iterations phi x0 =
+    let eps = 0.000001
+
+    if abs (x0 - (phi x0)) < eps then
+        x0
+    else
+        let next = phi x0
+        iterations phi next
+
+
+let newthon f f' x0 =
+    let phi x : float = x - (f x) / (f' x)
+    iterations phi x0
 // используйте функцию 'iterations'
 
 // Решите 3 уравнения (начиная со своего номера варианта) с использованием 3-х методов
-let f1 = ...
-let f2 = ...
-let f3 = ...
+let f1 x : float = 0.1 * (x ** 2.) - x * System.Math.Log x // #20
 
-let f1' = ...
-let f2' = ...
-let f3' = ...
+let f2 x : float = // #21
+    System.Math.Tan x - ((System.Math.Tan x) ** 3.) / 3.
+    + ((System.Math.Tan x) ** 5.) / 5.
+    - (1. / 3.)
 
-let phi1 = ...
-let phi2 = ...
-let phi3 = ...
+let f3 x : float = // #22
+    System.Math.Acos x - sqrt (1. - 0.3 * (x ** 3.))
 
-let main = 
-    printfn "%10.5f  %10.5f  %10.5f" (dichotomy f1 0. 1.) (iterations phi1 0.) (newthon f1 f1' 1.)
-    printfn "%10.5f  %10.5f  %10.5f" (dichotomy f2 0. 1.) (iterations phi2 0.) (newthon f2 f2' 1.)
-    printfn "%10.5f  %10.5f  %10.5f" (dichotomy f3 0. 1.) (iterations phi3 0.) (newthon f3 f3' 1.)
+
+let f1' x : float = 0.2 * x - System.Math.Log x - 1. // #20
+
+let f2' x : float = // #21
+    0.125 * (3. * System.Math.Cos 4. * x + 5.) * ((System.Math.Cos x) ** (-6.))
+
+let f3' x : float = // #22
+    (0.45 * (x ** 2.)) / (sqrt (1. - 0.3 * (x ** 3.))) - 1. / (sqrt (1. - x ** 2.))
+
+
+let phi1 x : float = System.Math.E ** (0.1 * x) // #20
+
+let phi2 x : float = // #21
+    System.Math.Atan((System.Math.Tan(x) ** 3.) / 3. - (System.Math.Tan(x) ** 5.) / 5. + 1. / 3.)
+
+let phi3 x : float = // #22
+    System.Math.Cos(sqrt (1. - 0.3 * (x ** 3.)))
+
+let main =
+    printfn "%10.5f  %10.5f  %10.5f" (dichotomy f1 1. 2.) (iterations phi1 1.5) (newthon f1 f1' 1.5)
+    printfn "%10.5f  %10.5f  %10.5f" (dichotomy f2 0. 0.8) (iterations phi2 0.4) (newthon f2 f2' 0.4)
+    printfn "%10.5f  %10.5f  %10.5f" (dichotomy f3 0. 1.) (iterations phi3 0.5) (newthon f3 f3' 0.5)
+
+main
--- Lab1/taylor.fsx
+++ Lab1/taylor.fsx
@@ -1,24 +1,53 @@
-// Print a table of a given function f, computed by taylor series
-
 // function to compute
-let f = sin
+let f x : float = float (System.Math.E ** (2. * x))
 
-let a = 0.0
-let b = 1.0
+let a = 0.1
+let b = 0.6
 let n = 10
 
-// Define a function to compute f using naive taylor series method
-let taylor_naive = f
+let rec iter f acc a b =
+    if a <= b then
+        iter f (f a acc) (a+1) b
+    else
+        acc
 
+let fact n = iter (*) 1 1 n
+
+let pown a = iter (fun x acc -> acc*a) 1. 1
+
+let rec taylor_naive' (x: float) n (acc: float) =
+    let eps = 0.000001
+
+    if abs (f x - acc) < eps then
+        acc, n
+    else
+        let new_Acc = acc + (pown (2. * x) n) / float (fact n)
+        taylor_naive' x (n + 1) new_Acc
+
+
+// Define a function to compute f using naive taylor series method
+let taylor_naive (x: float) = taylor_naive' x 0 0.
+
+let rec taylor' (x: float) n (prev: float) (acc: float) =
+    let eps = 0.000001
+
+    if abs (f x - acc) < eps then
+        acc, n
+    else
+        let new_Acc = acc + prev * 2. * x / float n
+        let new_Prev = prev * 2. * x / float n
+        taylor' x (n + 1) new_Prev new_Acc
 
 // Define a function to do the same in a more efficient way
-let taylor = f
+let taylor (x: float) = taylor' x 1 1. 1.
 
-let main =
-   for i=0 to n do
-     let x = a+(float i)/(float n)*(b-a)
-     printfn "%5.2f  %10.6f  %10.6f   %10.6f" x (f x) (taylor_naive x) (taylor x)
+let main1 =
+    for i = 0 to n do
+        let x = a + (float i) / (float n) * (b - a)
+        let x1, n1 = taylor_naive x
+        let x2, n2 = taylor x
+        printfn "%5.2f    %10.6f  %10.6f %d  %10.6f %d" x (f x) x1 n1 x2 n2
 // make sure to improve this table to include the required number of iterations
 // for each of the methods
 
-main
+main1
